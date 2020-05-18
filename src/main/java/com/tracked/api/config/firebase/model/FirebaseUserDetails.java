d2c61fc1b4f58935acca1c29550fd7056fa0bc92
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-18 21:23:26 +0200

    added FirebaseUserDetails class to hold firebase information

---
+++ src/main/java/com/tracked/api/config/firebase/model/FirebaseUserDetails.java
@@ -0,0 +1,28 @@
+package com.tracked.api.config.firebase.model;
+
+import com.google.firebase.auth.FirebaseToken;
+
+public class FirebaseUserDetails {
+	private FirebaseToken token;
+
+	public FirebaseUserDetails(FirebaseToken token) {
+		this.token = token;
+	}
+
+	public String getEmail() {
+		return token.getEmail();
+	}
+
+	public String getIssuer() {
+		return token.getIssuer();
+	}
+
+	public String getName() {
+		return token.getName();
+	}
+
+	public String getUid() {
+		return token.getUid();
+	}
+
+}
