e93c0e8eb5bf92bdc43894790cc8d4c0ecafe8d4
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-18 21:23:39 +0200

    implemented custom firebase exceptions

--- src/main/java/com/tracked/api/config/firebase/exception/FirebaseUserDoesNotExistException.java
+++ src/main/java/com/tracked/api/config/firebase/exception/FirebaseUserDoesNotExistException.java
@@ -1,11 +1,9 @@
-package com.tracked.api.service.exception;
+package com.tracked.api.config.firebase.exception;
 
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 
 public class FirebaseUserDoesNotExistException extends AuthenticationCredentialsNotFoundException {
-
     public FirebaseUserDoesNotExistException() {
-        super("User not found");
+        super("User Not Found");
     }
-
 }
---
+++ src/main/java/com/tracked/api/config/firebase/exception/InvalidFirebaseTokenException.java
@@ -0,0 +1,9 @@
+package com.tracked.api.config.firebase.exception;
+
+import org.springframework.security.authentication.BadCredentialsException;
+
+public class InvalidFirebaseTokenException extends BadCredentialsException {
+    public InvalidFirebaseTokenException() {
+        super("");
+    }
+}
