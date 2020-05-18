a8baa9d9a2f9bbedcc1bc681db8296bb0e1efb5f
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-18 21:24:29 +0200

    removed unused imports

--- src/main/java/com/tracked/api/service/UserService.java
+++ src/main/java/com/tracked/api/service/UserService.java
@@ -1,7 +1,5 @@
 package com.tracked.api.service;
 
-import com.tracked.api.model.User;
-import com.tracked.api.service.shared.RegisterUserInit;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
