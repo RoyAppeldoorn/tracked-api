2ca7533617f0e92b57ef13d90b1899cdd0c0c06a
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-21 16:52:30 +0200

    removed unused imports

--- src/main/java/com/tracked/api/controller/ControllerExceptionHandler.java
+++ src/main/java/com/tracked/api/controller/ControllerExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.tracked.api.controller;
 
 import com.tracked.api.utils.ApiError;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
