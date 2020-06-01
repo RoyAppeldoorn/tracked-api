53c3c866bf1e1ad130f2157091a2fa738369be70
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-01 20:33:02 +0200

    Only allow get requests for /tracklist

--- src/main/java/com/tracked/api/config/SecurityConfig.java
+++ src/main/java/com/tracked/api/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.tracked.api.security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/genre/**").permitAll()
-                .antMatchers("/tracklist/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/tracklist/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .cors()
