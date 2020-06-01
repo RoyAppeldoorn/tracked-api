2112c2086f749d5adc834cf71ba4cfa1011d6365
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-01 20:33:48 +0200

    get User object from spring security Principal

--- src/main/java/com/tracked/api/controller/TracklistController.java
+++ src/main/java/com/tracked/api/controller/TracklistController.java
@@ -1,12 +1,17 @@
 package com.tracked.api.controller;
 
 import com.tracked.api.model.Tracklist;
+import com.tracked.api.model.User;
 import com.tracked.api.model.projection.ITracklist;
 import com.tracked.api.service.TracklistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @RequestMapping("/tracklist")
 @RestController
@@ -26,8 +31,16 @@ public class TracklistController {
     }
 
     @PostMapping("/create")
-    public void createTracklist(@Valid @RequestBody Tracklist tracklist) {
-        tracklistService.create(tracklist);
+    public Tracklist createTracklist(@Valid @RequestBody Tracklist tracklist, Principal principal) {
+        if(principal == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN, "Not authenticated, create an account first");
+        }
+
+        User firebaseUser = (User) principal;
+        tracklist.setUser(firebaseUser);
+
+        return tracklistService.create(tracklist);
     }
 
 }
